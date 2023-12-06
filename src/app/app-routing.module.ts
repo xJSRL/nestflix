3f2c36e54ea162f564ede6b259cebe3ecc9ac643
xJSRL <sorealbueno@gmail.com>
2023-12-06 10:20:57 +0800

    added movie details and vid player

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainMoviesViewComponent } from './pages/main-movies-view/main-movies-view.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
 
 const routes: Routes = [
   {
     path: '', component: MainMoviesViewComponent
+  },
+  {
+    path: 'details/:id', component: MovieDetailsComponent
   },
  
 ];
